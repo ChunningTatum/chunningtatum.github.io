2790cfab26b2a62212af3ab94aeee056f3c4ed1e
Jun <chunhui@live.nl>
2024-05-20 13:33:56 +0200

    added some pages

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const scrollContainer = document.querySelector("main");
 
-
 scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
